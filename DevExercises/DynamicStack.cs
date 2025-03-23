bdd22c6e7099aa20ba6adf614d0717fddec2a1e4
MirraRaine <gadget.tamara@gmail.com>
2025-03-22 23:09:26 -0700

    Add DynamicStack and StackByPriorityQueuePair

---
+++ DevExercises/DynamicStack.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+namespace DevExercises
+{
+    public class DynamicStack : Stack
+    {
+        private int[] stack;
+        private int stackTopIndex;
+
+        public DynamicStack()
+        {
+            stackTopIndex = -1;
+            stack = new int[10];
+        }
+
+        public override void Push(object? obj)
+        {
+            int element = Convert.ToInt32(obj);
+            if (stackTopIndex < stack.Length - 1)
+            {
+                stack[++stackTopIndex] = element;
+            }
+            else
+            {
+                stack = extendStackSize();
+                stack[stackTopIndex++] = element;
+            }
+        }
+
+        public override object Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Empty stack.");
+            }
+            int element = stack[stackTopIndex];
+            stack[stackTopIndex] = 0;
+            stackTopIndex--;
+            return element;
+        }
+
+        public int Size()
+        {
+            return stackTopIndex + 1;
+        }
+
+        public bool IsEmpty()
+        {
+            return stackTopIndex < 0;
+        }
+
+        public override object Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Empty stack.");
+            }
+            return stack[stackTopIndex];
+        }
+
+        private int[] ExtendStackSize()
+        {
+            int[] stackWithExtendedSize = new int[stack.Length * 2];
+            for (int i = 0; i < stack.Length; i++)
+            {
+                stackWithExtendedSize[i] = stack[i];
+            }
+            return stackWithExtendedSize;
+        }
+    }
+}
---
+++ DevExercises/StackByPriorityQueuePair.cs
@@ -0,0 +1,72 @@
+﻿using CustomPair = DevExercises.Pair;
+
+namespace DevExercises
+{
+    public class StackByPriorityQueuePair
+    {
+        int count;
+        private readonly PriorityQueue<CustomPair> queue;
+
+        public void Push(int element)
+        {
+            count++;
+            queue.Push(new CustomPair(count, element));
+        }
+
+        public int Pop()
+        {
+            if (queue.IsEmpty())
+            {
+                throw new InvalidOperationException("Empty stack.");
+            }
+            count--;
+            return queue.Pop();
+        }
+
+        public int Top()
+        {
+            CustomPair topElement = queue.Top();
+            return topElement.Value;
+        }
+
+        public bool IsEmpty()
+        {
+            return queue.IsEmpty();
+        }
+    }
+
+    public class PriorityQueue<CustomPair>
+    {
+
+        public void Push(CustomPair n)
+        {
+        }
+
+        public int Pop()
+        {
+            return 1;
+        }
+
+        public CustomPair? Top()
+        {
+            return default(CustomPair);
+        }
+
+        public bool IsEmpty()
+        {
+            return true;
+        }
+    }
+
+    public class Pair
+    {
+        public Pair(int value, int key)
+        {
+            this.Value = value;
+            this.Key = key;
+        }
+
+        public int Value { get; set; }
+        public int Key { get; set; }
+    }
+}
