7cc8d1f4da4c50644ee89716fbb24ef21aff54d8
MirraRaine <gadget.tamara@gmail.com>
2025-04-05 13:41:40 -0700

    Add cvut homework 1

---
+++ UnivercityHomeworks/Hw1_StaticVsInstance.cs
@@ -0,0 +1,35 @@
+﻿namespace UniversityHomeworks
+{
+    public class Hw1_StaticVsInstance
+    {
+        // Static variable (shared among all instances)
+        public static int count = 1;
+
+        // Instance variable (unique to each instance)
+        public int countInstance = 1;
+
+        // Method that always returns true
+        public bool GetsTrueAlways()
+        {
+            return true;
+        }
+
+        // Static method that always returns false
+        public static bool GetsFalseAlways()
+        {
+            return false;
+        }
+
+        // Method that increments and returns the instance variable
+        public int IncrementsAndReturnsInstance()
+        {
+            return countInstance++;
+        }
+
+        // Method that increments and returns the static variable
+        public int IncrementsAndReturnsStaticVar()
+        {
+            return count++;
+        }
+    }
+}
---
+++ UniversityHomeworksTests/Hw1_StaticVsInstance_Tests.cs
@@ -0,0 +1,79 @@
+﻿using UniversityHomeworks;
+
+namespace UniversityHomeworksTests
+{
+    /// <summary>
+    /// Test class to validate functionality of the <see cref="Hw1_StaticVsInstance"/> class.
+    /// </summary>
+    [TestClass]
+    public sealed class Hw1_StaticVsInstance_Tests
+    {
+        [TestMethod]
+        public void GetsTrueAlways_ReturnsTrue()
+        {
+            // Arrange
+            var hw = new Hw1_StaticVsInstance();
+
+            // Act
+            bool result = hw.GetsTrueAlways();
+
+            // Assert
+            for (int i = 0; i < 10; i++)
+            {
+                Assert.IsTrue(result);
+            }
+        }
+
+        [TestMethod]
+        public void GetsFalseAlways_ReturnsFalse()
+        {
+            // Act
+            bool result = Hw1_StaticVsInstance.GetsFalseAlways();
+
+            // Assert
+            for (int i = 0; i < 10; i++)
+            {
+                Assert.IsFalse(result);
+            }
+        }
+
+        [TestMethod]
+        public void IncrementsAndReturnsInstance_IncreasesCorrectlyForSingleInstance()
+        {
+            // Arrange
+            var hw = new Hw1_StaticVsInstance();
+            int firstCallResult, secondCallResult;
+
+            // Act
+            firstCallResult = hw.IncrementsAndReturnsInstance();
+            secondCallResult = hw.IncrementsAndReturnsInstance();
+
+            // Assert
+            Assert.AreEqual(1, firstCallResult);
+            Assert.AreEqual(2, secondCallResult);
+        }
+
+        [TestMethod]
+        public void IncrementsAndReturnsStaticVar_IncreasesCorrectlyForMultipleInstances()
+        {
+            // Arrange
+            var hw = new Hw1_StaticVsInstance();
+            var hw2 = new Hw1_StaticVsInstance();
+
+            int firstInstanceFirstCall, firstInstanceSecondCall;
+            int secondInstanceFirstCall, secondInstanceSecondCall;
+
+            // Act
+            firstInstanceFirstCall = hw.IncrementsAndReturnsStaticVar();  // Should return 1
+            secondInstanceFirstCall = hw2.IncrementsAndReturnsStaticVar(); // Should return 2, not 1, even though it is called from hw2 for the first time
+            firstInstanceSecondCall = hw.IncrementsAndReturnsStaticVar();  // Should return 3, even though it is called 2nd time from hw; it was also called from hw2
+            secondInstanceSecondCall = hw2.IncrementsAndReturnsStaticVar(); // Should return 4, because it was called 2 times from hw, and 1 time from hw2; this is the 4th call
+
+            // Assert
+            Assert.AreEqual(1, firstInstanceFirstCall);
+            Assert.AreEqual(2, secondInstanceFirstCall);
+            Assert.AreEqual(3, firstInstanceSecondCall);
+            Assert.AreEqual(4, secondInstanceSecondCall);
+        }
+    }
+}
